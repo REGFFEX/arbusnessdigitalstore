375f87ddf6302f780b296c03e2a90d9318b9d813
Paulvy ONGARY <paulvyongary@gmail.com>
2026-08-22 20:31:25 +0100

    fix: configure Vercel SPA routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      ignored: ['**/src-tauri/**', '**/node_modules/**']
+    }
   }
 })
